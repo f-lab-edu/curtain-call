8dc548389f29967ba96181d764d23e5afb8058c6
forkyy-dev <forkyy.dev@gmail.com>
2025-12-02 23:17:16 +0900

    feat: SpringSecurity 기본 설정 추가

--- src/main/java/com/curtaincall/common/service/PasswordManager.java
+++ src/main/java/com/curtaincall/common/service/PasswordManager.java
@@ -1,17 +1,14 @@
 package com.curtaincall.common.service;
 
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
+@RequiredArgsConstructor
 @Component
 public class PasswordManager {
 
     private final PasswordEncoder passwordEncoder;
-
-    public PasswordManager() {
-        this.passwordEncoder = new BCryptPasswordEncoder();
-    }
 
     public String encrypt(String rawPassword) {
         return passwordEncoder.encode(rawPassword);
